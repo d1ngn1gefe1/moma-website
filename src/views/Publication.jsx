429777b396b6c289b4ff016f3d65189daa350755
Linden Li <lindenli@stanford.edu>
2022-12-27 16:46:07 -0500

    Add NeurIPS paper

--- src/views/Publication.jsx
+++ src/views/Publication.jsx
@@ -6,6 +6,7 @@ import {
   Button
 } from 'react-bootstrap';
 import MomaPubImg from '../assets/img/moma-publication.png';
+import MomaLrgImg from '../assets/img/spotlight_momalrg.jpg';
 
 class Publication extends Component {
   constructor(props) {
@@ -31,6 +32,23 @@ class Publication extends Component {
               <p className="publication-moma-author">
                 Zelun Luo*, Wanze Xie*, Siddharth Kapoor, Yiyun Liang, Michael Cooper,
                 Juan Carlos Niebles, Ehsan Adeli, Li Fei-Fei
+              </p>
+              <Button variant="moma-primary" size="moma-lg" > View Paper </Button>
+            </Col>
+          </Row>
+          <Row>
+          </Row>
+
+          <br></br>
+
+          <Row>
+            <Col md={{ span: 3, order: 1}}>
+              <img className="publication-moma-img" src={MomaLrgImg} alt="moma-publication" />
+            </Col>
+            <Col md={{ span: 7, order: 2}}>
+              <p className="publication-moma-title"> MOMA-LRG: Language-Refined Graphs for Multi-Object Multi-Actor Activity Parsing </p>
+              <p className="publication-moma-author">
+              Zelun Luo, Zane Durante*, Linden Li*, Wanze Xie, Ruochen Liu, Emily Jin, Zhuoyi Huang, Lun Yu Li, Jiajun Wu, Juan Carlos Niebles, Ehsan Adeli, Li Fei-Fei
               </p>
               <Button variant="moma-primary" size="moma-lg" > View Paper </Button>
             </Col>
